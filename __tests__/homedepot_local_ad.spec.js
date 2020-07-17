01648bbd21e66ba488cee2b601d1db686e1ce37c
jessica-lau <jessica.lau2@gmail.com>
2020-07-17 15:49:32 -0400

    add test script for viewing local ad

---
+++ __tests__/homedepot_local_ad.spec.js
@@ -0,0 +1,63 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By;
+require('selenium-webdriver/testing');
+require('dotenv').config();
+var assert = require('assert');
+let chrome = require('selenium-webdriver/chrome');
+
+describe("Home Page", () => {
+    let driver;
+
+    before(async () => {
+        var capabilities = {
+            'browserName': 'Chrome',
+            'browser_version': '60.0',
+            'os': 'Windows',
+            'os_version': '10',
+            'resolution': '1920x1080',
+            'browserstack.user': process.env.browserstack_user,
+            'browserstack.key': process.env.browserstack_key,
+            'name': 'B Sample Test'
+        };
+        //Note: run test on browserstacker:
+
+        // var builder = new webdriver.Builder().
+        //   usingServer('http://hub-cloud.browserstack.com/wd/hub').
+        //   withCapabilities(capabilities);
+        // driver = await builder.build();
+
+        //Note: run test on local broswer:
+        let localChromeOption = new chrome.Options();
+        localChromeOption.addArguments("--start-maximized");
+
+        driver = await new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .setChromeOptions(localChromeOption)
+            .build();
+
+        console.log("started testing");
+        //driver.manage().window().maximize();
+        await driver.get('http://www.homedepot.com');
+
+    });
+
+    after(async () => {
+        await driver.quit();
+    });
+
+    describe("Header", () => {
+        it("should exist", async () => {
+            let title = await driver.getTitle()
+            console.log(assert.equal(title, "The Home Depot"));
+        });
+
+        it("view local ad", async () => {
+            let localAd = await driver.findElement(By.linkText(`Local Ad`));
+            let isLocalAdDisplayed = await localAd.isDisplayed();
+            assert.equal(isLocalAdDisplayed, 1);
+            await localAd.click();
+            await driver.sleep(6000);
+        });
+
+    });
+});
